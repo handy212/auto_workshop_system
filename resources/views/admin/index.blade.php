3347efa17d83272d78912d97339d6468e2a4a495
handy212 <handysck3@gmail.com>
2023-08-27 19:27:06 +0100

    update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
 
         <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
             <div>
-                <h4 class="mb-3 mb-md-0">Welcome to Automobile Workshop System</h4>
+                <h4 class="mb-3 mb-md-0">Welcome to Easy Workshop</h4>
             </div>
             <div class="d-flex align-items-center flex-wrap text-nowrap">
                 <div class="input-group flatpickr wd-200 me-2 mb-2 mb-md-0" id="dashboardDate">
@@ -214,7 +214,7 @@
         </div> <!-- row -->
 
         <div class="row">
-            <div class="col-lg-7 col-xl-8 grid-margin stretch-card">
+            <div class="col-lg-7 col-xl-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-baseline mb-2">
@@ -246,57 +246,6 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-5 col-xl-4 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between align-items-baseline">
-                            <h6 class="card-title mb-0">Cloud storage</h6>
-                            <div class="dropdown mb-2">
-                                <a type="button" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-haspopup="true"
-                                   aria-expanded="false">
-                                    <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                            data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                            data-feather="edit-2" class="icon-sm me-2"></i> <span
-                                            class="">Edit</span></a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                            data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                            data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                            data-feather="download" class="icon-sm me-2"></i> <span
-                                            class="">Download</span></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="storageChart"></div>
-                        <div class="row mb-3">
-                            <div class="col-6 d-flex justify-content-end">
-                                <div>
-                                    <label
-                                        class="d-flex align-items-center justify-content-end tx-10 text-uppercase fw-bolder">Total
-                                        storage <span class="p-1 ms-1 rounded-circle bg-secondary"></span></label>
-                                    <h5 class="fw-bolder mb-0 text-end">8TB</h5>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div>
-                                    <label class="d-flex align-items-center tx-10 text-uppercase fw-bolder"><span
-                                            class="p-1 me-1 rounded-circle bg-primary"></span> Used storage</label>
-                                    <h5 class="fw-bolder mb-0">~5TB</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="d-grid">
-                            <button class="btn btn-primary">Upgrade storage</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- row -->
 
         <div class="row">
             <div class="col-lg-5 col-xl-4 grid-margin grid-margin-xl-0 stretch-card">
--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,21 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
-
+//Admin Group Middleware
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/save', [AdminController::class, 'AdminProfileSave'])->name('admin.profile.save');
 
 });
-
+// Customer Group Muddleware
 Route::middleware(['auth', 'role:customer'])->group(function () {
     Route::get('/customer/dashboard', [CustomerController::class, 'CustomerDashboard'])->name('customer.dashboard');
 
 });
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
 
 
