92257d0910913f61287e2579b6a42f9228731a7b
handy212 <handysck3@gmail.com>
2023-08-31 12:37:15 +0000

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -41,20 +41,18 @@ class AdminController extends Controller
         $data->phone =$request->phone;
         $data->address =$request->address;
 
-//        if ($request->file('photo')){
-//            $file = $request->file('photo');
-//            $filename = date('YmdHi').$file->getClientOriginalName();
-//            $file->move(public_path('upload/admin_images'),$filename);
-//            $data['photo'] = $filename;
-//
-//        }
         if ($request->file('photo')) {
             $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/'.$data->photo));
             $filename = date('YmdHi') . $file->getClientOriginalName();
             $file->move(public_path('upload/admin_images'), $filename);
             $data->photo = $filename; // Use object property syntax here
         }
         $data->save();
-        return redirect()->back();
+        $notification = array(
+            'message' => 'Profile Updated Successfully',
+            'alert-type' =>'success'
+        );
+        return redirect()->back()->with($notification);
     }
 }
